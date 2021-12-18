4fada9eeaea6cd67cf0ccb30d52477953f531db5
Katie <costantinkp@gmail.com>
2021-12-17 21:29:45 -0500

    still coming up a bit off- work on it more tomorrow

--- script.js
+++ script.js
@@ -7,40 +7,29 @@ function calcPay() {
 
     const x = start.search(":");
     const startHour = start.slice(0,x);
-    console.log(startHour)
+    //console.log(startHour)
     const startMin = start.slice(x+1);
     //console.log(startMin);
     const startDec =  ((startMin*100)/60)/100;
-    console.log(startDec);
+    //console.log(startDec);
     const startTime = parseFloat(startHour)+parseFloat(startDec);
-    console.log(startTime)
-
-
-
-    //const startTime = parseInt((startHour+startDec)/100);
-    //need if statement here bc if its right on the hour this is where it mis calculates
-    //console.log(startTime);
+    //console.log(startTime)
 
     const y = end.search(":");
     const endHour = end.slice(0,x); 
     const endMin = end.slice(x+1);
-    const endDec =  (endMin*100)/60;
-    const endTime = parseInt((endHour+endDec)/100);
+    const endDec =  ((startMin*100)/60)/100;
+    //need if statement here if its 00 on the hour- then this not the correct equation
+    const endTime = parseInt(endHour)+parseInt(endDec);
+    //console.log(endTime);
 
-   //console.log(startTime);
-   //console.log(endTime);
+    const hours = (endTime-startTime);
+    //add if statement that if the number is positive its fine, if negative make positive
+    //console.log(hours);
 
-   // const hoursX = (endTime-startTime);
-    //console.log(hoursX);
+    const total = hours*parseFloat(pay);
+    console.log(total);
 
-/**
- * !this code works if going from am to pm- need if statement if 2 pms
- */
-    //const total = (endTime-startTime)*15;
-    //const positiveTotal = Math.round(Math.abs(total));
-
-    //console.log(total);
 
 };
 
-//const minInt = (endMin*100)/60
