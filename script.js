07579e747d3ffe1bdf83478b3388eccf58628cbb
Katie <costantinkp@gmail.com>
2021-12-17 21:09:02 -0500

    startTime algo is working - forgot to parseFloat the strings

--- script.js
+++ script.js
@@ -2,13 +2,45 @@ const calc = document.getElementById('button').addEventListener('click', calcPay
 
 function calcPay() {
     let start = document.getElementById("timeStart").value;
-    const end = document.getElementById("timeEnd").value;
+    let end = document.getElementById("timeEnd").value;
     const pay = document.getElementById("pay").value;
 
     const x = start.search(":");
-    start = start.slice(0,x) + start.slice(x+1);
+    const startHour = start.slice(0,x);
+    console.log(startHour)
+    const startMin = start.slice(x+1);
+    //console.log(startMin);
+    const startDec =  ((startMin*100)/60)/100;
+    console.log(startDec);
+    const startTime = parseFloat(startHour)+parseFloat(startDec);
+    console.log(startTime)
 
-    //const final = ((parseInt(end)-parseInt(start))*parseInt(pay));
-    console.log(start)
+
+
+    //const startTime = parseInt((startHour+startDec)/100);
+    //need if statement here bc if its right on the hour this is where it mis calculates
+    //console.log(startTime);
+
+    const y = end.search(":");
+    const endHour = end.slice(0,x); 
+    const endMin = end.slice(x+1);
+    const endDec =  (endMin*100)/60;
+    const endTime = parseInt((endHour+endDec)/100);
+
+   //console.log(startTime);
+   //console.log(endTime);
+
+   // const hoursX = (endTime-startTime);
+    //console.log(hoursX);
+
+/**
+ * !this code works if going from am to pm- need if statement if 2 pms
+ */
+    //const total = (endTime-startTime)*15;
+    //const positiveTotal = Math.round(Math.abs(total));
+
+    //console.log(total);
+
 };
 
+//const minInt = (endMin*100)/60
